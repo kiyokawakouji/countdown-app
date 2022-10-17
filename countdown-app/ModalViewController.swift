56adf7dcc6f378aafb1b91231eca9bd6334b29d7
kiyokawakouji <kohji12k@gmail.com>
2022-10-18 01:09:56 +0900

    TODOコメント書いた

--- countdown-app/ModalViewController.swift
+++ countdown-app/ModalViewController.swift
@@ -14,13 +14,14 @@ class ModalViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // TODO: ボタンの整形(角丸・サイズ・ボタン間の余白)
         No.backgroundColor = UIColor(red: 0.24, green: 0.36, blue: 0.14, alpha: 1.0)
         Yes.backgroundColor = UIColor(red: 0.24, green: 0.36, blue: 0.14, alpha: 1.0)
     }
 
 
-
+    // TODO: Yes/Noで別アクション作成する
+    // TODO: Yesの場合カウントダウンを実行する
     @IBAction func dismissModal(_ sender: Any) {
         self.dismiss(animated: true,completion: nil)
     }
--- countdown-app/ViewController.swift
+++ countdown-app/ViewController.swift
@@ -16,9 +16,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // TODO: ボタンの整形(角丸・サイズ)
         button.backgroundColor = UIColor(red: 0.24, green: 0.36, blue: 0.14, alpha: 1.0)
     }
+    // TODO: カウントダウンしたらcountRemainingLabelのカウントを減らす
+    // TODO: あと0回でButtonを押すと「今日はもうおしまいです」というモーダルを表示する
+    // TODO: 「今日はもうおしまいです」モーダルでもう一度遊べるドンを押すとViewControllerへ遷移する
+    // TODO: もう一度遊べるドンを押すとViewControllerへ遷移するとカウントダウンがリセットさせれる
+
 
     @IBAction  func showModal() {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
